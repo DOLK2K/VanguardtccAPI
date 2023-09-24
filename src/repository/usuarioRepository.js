e7b0e8a682a861e0ff68a48809160b99dcc87ec9
Vinícius <Marcosferreira.1234filho@gmail.com>
2023-09-24 18:54:13 -0300

    ok

--- src/repository/usuarioRepository.js
+++ src/repository/usuarioRepository.js
@@ -2,14 +2,16 @@ import {con} from './connection.js'
 
 export  async function login (email, senha) {
     const  comando = 
-    `select id_usuario	id,
-    nm_usuario	nome,       
-    ds_email	email
-    from tb_usuario
+    `select id_cadastro  	id,      
+    ds_email	email,
+    nr_telefone telefone
+    from tb_cadastro
     where ds_email =	'?'
     and ds_senha	=	'?`
 
     const [linhas] = await con.query(comando, [email, senha])
+
+    console.log(linhas)
     return linhas; 
 }
 
