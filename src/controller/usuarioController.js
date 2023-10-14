f388626737c3faacbbf0c80802c5cfc80473c37c
Vinícius <Marcosferreira.1234filho@gmail.com>
2023-10-14 02:13:51 -0300

    endpoint de cadastro feito com sucesso

--- src/controller/usuarioController.js
+++ src/controller/usuarioController.js
@@ -1,26 +1,44 @@
 import { Router } from "express";
-import {login} from '../repository/usuarioRepository.js'
-const server = Router()
+import {Cadastro, login} from '../repository/usuarioRepository.js'
+const endpoint = Router()
 
 
-server.get('/usuario', async (req,resp) => {
+endpoint.post('/usuario', async (req,resp) => {
     try {
+        let usuario = req.body 
+        
 
-        const resposta = await login(email, senha)
-        if(!resposta) {
-            throw new Error('credenciais invalidas')
+        if(!usuario.email) {
+            throw new Error('email obrigatorio')
         }
-        resp.send(resposta)
 
+        if(  (!usuario.telefone)) {
+             throw new Error('numero invalido ou campo vazio')
+        }
+
+       if(!usuario.senha) {
+        throw new Error ('senha obrigatório')
+       }
+
+       if(!usuario.nome_completo) {
+        throw new Error ('Nome obrigatorio')
+       }
+       
+       let r = await Cadastro(usuario)
+       resp.send(r)
+
+       if(!usuario) {
+        throw new Error('credenciais invalidas ou está vazio os campos')
+       }
        
     }catch(err) {
-        resp.status(401).send({
+        resp.status(500).send({
             erro:err.message
         });
     }
 })
 
-server.post('/usuario/login', async (req, resp) => {
+endpoint.post('/usuario/login', async (req, resp) => {
     try{
         const {email, senha } = req.body
 
@@ -44,6 +62,6 @@ server.post('/usuario/login', async (req, resp) => {
 
 
 
-export default server;
+export default endpoint;
 
 
--- src/repository/usuarioRepository.js
+++ src/repository/usuarioRepository.js
@@ -1,5 +1,18 @@
 import {con} from './connection.js'
 
+
+export async function Cadastro (cliente) {
+    const comando2 = `
+    INSERT INTO tb_cadastro(  ds_email, nr_telefone, ds_senha, nm_nome_completo)
+				value (?, ?, ?, ?);`
+
+    const [resposta] = await con.query(comando2, [cliente.email, cliente.telefone, cliente.senha, cliente.nome_completo])
+    cliente.id = resposta.insertId;
+
+    return cliente;
+
+}
+
 export  async function login (email, senha) {
     const  comando = 
     `SELECT id_login AS id,
